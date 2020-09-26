9560cdb679d2627fcb0ecf479beec5facee68f03
Tristan Rivoallan <tristan.rivoallan@laposte.fr>
2020-09-26 23:54:40 +0200

    oop

--- directus/public/docs/index.php
+++ directus/public/docs/index.php
@@ -58,7 +58,7 @@
 
             <h1 class="first ui dividing header">IVRESSE</h1>
             <blockquote>
-                <span style="color:#<?php echo random(0, 9) ?><?php echo random(0, 9) ?><?php echo random(0, 9) ?>;">Toucher du doigt la substantifique moëlle</span> <span style="color:#<?php echo random(0, 9) ?><?php echo random(0, 9) ?><?php echo random(0, 9) ?>;">assister au triomphe du signal sur le bruit</span> <span style="color:#<?php echo random(0, 9) ?><?php echo random(0, 9) ?><?php echo random(0, 9) ?>;">manifester un désintérêt total pour les flacons</span> <span style="color:#<?php echo random(0, 9) ?><?php echo random(0, 9) ?><?php echo random(0, 9) ?>;">transpercer des rêves à grands coups d'étoiles</span>
+                <span style="color:#<?php echo rand(0, 9) ?><?php echo rand(0, 9) ?><?php echo rand(0, 9) ?>;">Toucher du doigt la substantifique moëlle</span> <span style="color:#<?php echo rand(0, 9) ?><?php echo rand(0, 9) ?><?php echo rand(0, 9) ?>;">assister au triomphe du signal sur le bruit</span> <span style="color:#<?php echo rand(0, 9) ?><?php echo rand(0, 9) ?><?php echo rand(0, 9) ?>;">manifester un désintérêt total pour les flacons</span> <span style="color:#<?php echo rand(0, 9) ?><?php echo rand(0, 9) ?><?php echo rand(0, 9) ?>;">transpercer des rêves à grands coups d'étoiles</span>
             </blockquote>
         </div>
 
